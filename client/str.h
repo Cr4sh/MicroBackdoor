12b1b234f75c54673644517e524b51024047a65a
Cr4sh <cr4sh0@gmail.com>
2021-05-06 20:54:16 +0700

    clean up mess

--- client/str.h
+++ client/str.h
@@ -1,9 +1,3 @@
 
-extern "C"
-{
-//void *memcpy(void *dst, const void *src, size_t size);
-//void *memset(void *mem, unsigned char val, size_t size);
-}
-
 int str_item_count(char *str, char sep);
 BOOL str_item_get(char *str, char sep, int num, char **buff);
